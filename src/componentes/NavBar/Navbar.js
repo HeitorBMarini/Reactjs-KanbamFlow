78e1d2cf4addaa6b85efff90a0a3dc122f388e63
Heitor Borba Marini <97682988+heitorborbamarini@users.noreply.github.com>
2022-06-16 10:03:04 -0300

    Initial commit

---
+++ src/componentes/NavBar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <span> React Kanbam</span>
+    </nav>
+  );
+}
